51467ddb9e04131dcfb1821bfccbdb97c1743c87
wallperes <wpscpii@gmail.com>
2025-07-19 20:23:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 from datetime import datetime, timedelta
-import SGS  # Importação corrigida
+from sgs import SGS
 
 def format_currency(value):
     """Formata valores no padrão brasileiro R$ 1.234,56"""
